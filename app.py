7db01e53d5b875b293ad1576b8dfe596f32b8525
Eric <ericncnl3742@gmail.com>
2024-01-05 01:24:10 +0800

    add value submit to flask

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ database = SQLAlchemy(app)
 @app.route('/customers')
 def customers():
     theme = request.args.get('theme') or 'light'
+    search = request.args.get('search') or ''
+    print(search)
     data = [
         {'name': 'John Doe', 'email': '2h4gK@example.com', 'phone': '123-456-7890'},
         {'name': 'Keele', 'email': 'bonbonbomb@example.com', 'phone': '114-514-1919810'},
@@ -19,14 +21,14 @@ def customers():
 
 @app.route('/bookings')
 def bookings():
-    if request.method == 'GET':
-        theme = request.args.get('theme') or 'light'
-
-        data = [
-            {'name': 'John Doe', 'room': '101', 'checkin': '2022-01-01', 'checkout': '2022-01-02'},
-            {'name': 'Jane Doe', 'room': '102', 'checkin': '2022-01-03', 'checkout': '2022-01-04'},
-        ]
-        return render_template('bookings.html', theme=theme, bookings=data)
+    theme = request.args.get('theme') or 'light'
+    search = request.args.get('search') or ''
+    print(search)
+    data = [
+        {'name': 'John Doe', 'room': '101', 'checkin': '2022-01-01', 'checkout': '2022-01-02'},
+        {'name': 'Jane Doe', 'room': '102', 'checkin': '2022-01-03', 'checkout': '2022-01-04'},
+    ]
+    return render_template('bookings.html', theme=theme, bookings=data)
 
 
 @app.route('/')
